7e6555167985d0b55d855cbcba8dc780e30af326
Maya Harper <maya@girlfag.gay>
2026-06-08 21:18:52 -0500

    add code, test CI/CD

---
+++ include/gloop.h
@@ -0,0 +1,13 @@
+//
+// Created by maya on 6/8/26.
+//
+
+#ifndef GLOOP_GLOOP_H
+#define GLOOP_GLOOP_H
+
+
+class gloop {
+};
+
+
+#endif //GLOOP_GLOOP_H
---
+++ src/gloop.cpp
@@ -0,0 +1,2 @@
+
+#include "../include/gloop.h"
